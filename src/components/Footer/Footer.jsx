52ee8f64c756c3e159e127af4fef65a8e92080c1
DevAshgeek <ashutoshyad3105@gmail.com>
2025-02-25 21:43:40 +0530

    updated footer links

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,33 +2,33 @@ import React from "react";
 import "./Footer.css";
 import { socialdetails } from "../../utils/data";
 const Footer = ({ location, country }) => {
-  const navigateToSec = (e) => {
-    const pressedbtn = e.target.getAttribute("id");
-    let elementtotarget = null;
-    switch (pressedbtn) {
-      case "todbtn":
-        elementtotarget = document.getElementById("todweather");
-        break;
-      case "hrbtn":
-        elementtotarget = document.getElementById("hourlyweather");
-        break;
-      case "dlybtn":
-        elementtotarget = document.getElementById("dailyweather");
-        break;
-      case "mtlybtn":
-        elementtotarget = document.getElementById("mtlyweathcont");
-        break;
-      default:
-        console.error("Invalid element", e.target);
-        return;
-    }
+  // const navigateToSec = (e) => {
+  //   const pressedbtn = e.target.getAttribute("id");
+  //   let elementtotarget = null;
+  //   switch (pressedbtn) {
+  //     case "todbtn":
+  //       elementtotarget = document.getElementById("todweather");
+  //       break;
+  //     case "hrbtn":
+  //       elementtotarget = document.getElementById("hourlyweather");
+  //       break;
+  //     case "dlybtn":
+  //       elementtotarget = document.getElementById("dailyweather");
+  //       break;
+  //     case "mtlybtn":
+  //       elementtotarget = document.getElementById("mtlyweathcont");
+  //       break;
+  //     default:
+  //       console.error("Invalid element", e.target);
+  //       return;
+  //   }
 
-    if (elementtotarget) {
-      elementtotarget.scrollIntoView({ behavior: "smooth", block: "start" });
-    } else {
-      console.error("Target element not found:", e.target);
-    }
-  };
+  //   if (elementtotarget) {
+  //     elementtotarget.scrollIntoView({ behavior: "smooth", block: "start" });
+  //   } else {
+  //     console.error("Target element not found:", e.target);
+  //   }
+  // };
   return (
     <div className="wrapper">
       <hr className="fthr" />
@@ -38,7 +38,7 @@ const Footer = ({ location, country }) => {
           {location}, {country}
         </span>
       </div>
-      <div className="nav">
+      {/* <div className="nav">
         <span className="navbtns" id="todbtn" onClick={navigateToSec}>
           today
         </span>
@@ -51,7 +51,7 @@ const Footer = ({ location, country }) => {
         <span className="navbtns" id="mtlybtn" onClick={navigateToSec}>
           monthly
         </span>
-      </div>
+      </div> */}
       <div className="author">
         <span>Designed and developed with ❤️ by Ashutosh Yadav</span>
       </div>
